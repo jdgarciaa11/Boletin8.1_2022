4f7e19aaf2ccb4fa0875e5c4a10d864ed9bbb7e8
Jesus Garcia <jesusd.garcia@iesnervion.es>
2022-03-04 10:36:46 +0100

    Terminado EJ5

--- src/EJ5/Main.java
+++ src/EJ5/Main.java
@@ -9,16 +9,23 @@ public class Main {
     private static final int[] RANDOM = generadorRandom();
     public static void main(String[] args) {
         int numero = 0, cont = 0;
+        int[] numeroArray = new int[LONGITUD];
+        String resultado;
         Scanner scan = new Scanner(System.in);
         do {
             do {
                 System.out.println("Introduzca 4 numeros: ");
                 numero = validarDatosNumericos();
             } while (numero < 999);
-
+            numeroArray = conversorArray(numero);
+            if (comprobador(numeroArray).equals("1111")){
+                System.out.println("Enorabuena! Has ganado");
+            } else {
+                resultado = comprobador(conversorArray(numero));
+                System.out.println(resultado);
+            }
             cont++;
         } while (cont < 13);
-        System.out.println("Introduzca 4 numeros: ");
 
     }
 
@@ -32,24 +39,44 @@ public class Main {
     }
 
     private static int[] conversorArray(int numero) {
-        String cadenaNumeros = Integer.toString(numero);
+        String cadenaNumeros = Integer.toString(numero) ;
         int[] array = new int[LONGITUD];
         for (int i = 0; i < LONGITUD; i++) {
-            array[i] = (int) cadenaNumeros.charAt(i);
+            array[i] = ((int) cadenaNumeros.charAt(i))-48;
         }
         return array;
     }
 
     private static String comprobador(int[] array){
         StringBuilder resultado = new StringBuilder();
+        boolean parar = false;
         for (int i = 0; i < LONGITUD; i++){
-            for (int j = 0; j < LONGITUD; j++){
-                if(array[i] == RANDOM[j]){
+            do{
+                if(array[i] == RANDOM[i]) {
                     resultado.append('1');
+                    parar = true;
+                } else if(encontrado(array[i])){
+                    resultado.append('0');
+                    parar = true;
+                } else {
+                    resultado.append('X');
+                    parar = true;
                 }
-            }
+            }while(!parar);
+            parar = false;
         }
         return resultado.toString();
+    }
+
+    private static boolean encontrado(int origen){
+        boolean encontrado = false;
+        for(int i = 0; i < LONGITUD; i++){
+            if (RANDOM[i] == origen) {
+                encontrado = true;
+                break;
+            }
+        }
+        return encontrado;
     }
 
     private static int validarDatosNumericos() {
